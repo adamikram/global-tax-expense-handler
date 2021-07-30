7c3a13caef90f36312d2907005b6087431b9154d
Adam Ikram <62781807+adamikram@users.noreply.github.com>
2021-07-30 00:09:53 -0600

    c

---
+++ scripts/gui.py
@@ -0,0 +1,27 @@
+import tkinter as tk
+
+class labelList
+
+
+
+
+
+
+window = tk.Tk()
+programFrame = tk.Frame(master=window,bg="green")
+programFrame.pack(fill=tk.BOTH,expand=True)
+
+titleFrame = tk.Frame(master=programFrame,height=50, bg="blue")
+titleFrame.pack(fill=tk.X,side=tk.TOP)
+
+actionFrame = tk.Frame(master=programFrame,bg="yellow")
+actionFrame.pack(fill=tk.BOTH,side=tk.TOP,expand=True)
+
+titleLabel = tk.Label(master=titleFrame,text="Expense Consolidator")
+titleLabel.pack(fill=tk.BOTH,side=tk.TOP,expand=True)
+
+expenseTypeColumnTitle = tk.Label(master=actionFrame,text="Expense Types")
+expenseTypeColumnTitle.grid(row=0,column=0)
+
+
+window.mainloop()
