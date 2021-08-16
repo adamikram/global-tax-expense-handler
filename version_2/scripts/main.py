823dac42ec4ae908f7325efd6d6dd3d0b2a2a909
Adam Ikram <62781807+adamikram@users.noreply.github.com>
2021-08-16 00:57:54 -0600

    ed

--- version_2/scripts/main.py
+++ version_2/scripts/main.py
@@ -9,12 +9,20 @@ import pandas as pd
 class CsvParse:
     def __init__(self, file_path):
         self.df = pd.read_csv(file_path)
+        self.df = self.df.fillna('')
 
     def get_headings(self):
         return self.df.columns
 
     def get_column(self,column):
-        return self.df[column].tolist()
+        # arr = self.df[column].fillna('')
+        arr = self.df[column].tolist()
+        while True:
+            try:
+                arr.remove('')
+            except ValueError:
+                break
+        return arr
     def save(self,path):
         pass
 
@@ -42,18 +50,41 @@ class WordBank(CsvParse):
         for column in self.get_headings():
             arr = self.get_column(column)
             code = arr[0]
-            del arr[0]
+
             newType = ExpenseType(column, code, arr)
             self.expenseTypes.append(newType)
 
+    def get_operating_expenses(self):
+        arr = []
+        for expenseType in self.expenseTypes:
+            if expenseType.operating_expense != "":
+                arr.append(expenseType.operating_expense)
+        return arr
 
+    def remove_keyword(self,expenseType, value):
+        for type in self.expenseTypes:
+            if type.operatingExpense == expenseType:
+                type.remove_keyword(value)
+                break
 
+    def add_keyword(self,expenseType, value):
+        for type in self.expenseTypes:
+            if type.operatingExpense == expenseType:
+                type.remove_keyword(value)
+                break
 
+    def get_word_bank(self,expenseType):
+        for type in self.expenseTypes:
+            if type.operating_expense == expenseType:
+                return type.wordBank
 
 class MainProgram():
     def __init__(self,word_bank_path):
         self.wordBank = WordBank(word_bank_path)
-        pass
+        for expenseType in self.wordBank.get_operating_expenses():
+            # print(expenseType)
+            # print(self.wordBank.get_word_bank(expenseType))
+            pass
 
 
 wordBankPath = r'C:\Users\Ahmed\OneDrive\Desktop\global-tax-expense-handler\version_2\data\newWordBank.csv'
